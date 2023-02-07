c0cd15ca67372cf953897aa190bf75999a976da3
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-07 01:23:31 -0800

    Returns the tail of an array: everything except for the first item (head) of the provided array.

--- tail.js
+++ tail.js
@@ -17,7 +17,6 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
