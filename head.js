075aeac88363efc93d5f177b9c10264da5d18831
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-07 01:18:23 -0800

    Returns first element of array as itself

--- head.js
+++ head.js
@@ -17,4 +17,5 @@ const head = function(array) {
   return array[0];
 };
 
-console.assert(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
