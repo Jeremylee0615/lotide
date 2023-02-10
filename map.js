50b8366188eab2269d8a7dea9b326db5afbdeacf
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-09 22:03:42 -0800

    Implement Map

---
+++ map.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && eqArrays(arr1[i], arr2[i]) === false) {
+      return false;
+    } if (!(Array.isArray(arr1[i])) && arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+//*****************Example Code*********************//
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results1, [ 'f', 'y', 'q', 'g', 't' ]);
+//*****************My Implementation****************//
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+
+const results3 = map(words, word => word[3]);
+assertArraysEqual(results3, ['u', 't', ' ', 'o',' ']);
+
+const results5 = map(words, word => word.split('o').join(''));
+assertArraysEqual(results5, [ 'grund', 'cntrl', 't', 'majr', 'tm' ]);
+
