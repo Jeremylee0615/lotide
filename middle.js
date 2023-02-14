b2edbe3e0be323e3c79213e01642311185f8cda3
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-14 00:16:28 -0800

    Refactored for module implement

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let value = 0; value < arr1.length; value += 1) {
-    if ((Array.isArray(arr1[value]) || Array.isArray(arr2[value])) && eqArrays(arr1[value], arr2[value]) === false) {
-      return false;
-    } else if (Array.isArray(arr1[value]) || Array.isArray(arr2[value])) {
-      eqArrays(arr1[value], arr2[value]);
-    } else if (arr1[value] !== arr2[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅  Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(arr) {
   if (arr.length <= 2) {
     return [];
@@ -32,10 +8,4 @@ const middle = function(arr) {
   }
 };
 
-
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle; 
