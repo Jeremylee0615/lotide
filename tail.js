444b23cacfe50bd4a079d3a199c0a0e3e2fb29a4
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-13 23:26:20 -0800

    Refactored for module implement

--- tail.js
+++ tail.js
@@ -1,23 +1,5 @@
-// AssertEqual Tester
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-     
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  
-    return true;
-  } else {
-    if (actual !== expected) {
-      console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-    }
-    return false;
-  }
-};
-
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
