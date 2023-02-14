a4f2573969f0102cb7b39616ca60d71d8aa9d98e
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-14 00:02:31 -0800

    Refactored for module implement

--- eqArrays.js
+++ eqArrays.js
@@ -1,23 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-     
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  
-    return true;
-  } else {
-    if (actual !== expected) {
-      console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-    }
-    return false;
-  }
-};
-
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } 
   for (let i = 0; i < arr1.length; i++) {
-    if (Array.isArray(arr1[i]) && eqArrays(arr1[i], arr2[i]) === false) {
+    if (Array.isArray(arr1[i]) && arr1[i] !== arr2[i]) {
       return false;
     } if (!(Array.isArray(arr1[i])) && arr1[i] !== arr2[i]) {
         return false;
@@ -26,8 +12,5 @@ const eqArrays = function(arr1,arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
-
-
-
+module.exports = eqArrays;
