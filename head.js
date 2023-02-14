3d8117a7870138c18b63d479cc3110c5c63ed248
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-13 23:25:19 -0800

    Refactored for module implement

--- head.js
+++ head.js
@@ -1,21 +1,7 @@
-// AssertEqual Tester
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-     
-    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
-  
-    return true;
-  } else {
-    if (actual !== expected) {
-      console.log(`🛑🛑🛑  Assertion Failed: ${actual} !== ${expected}`);
-    }
-    return false;
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
+
